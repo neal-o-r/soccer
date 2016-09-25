9401609ef5e441564959884cd49393da8b4c60a4
neal-o-r <neal@n-o-r.xyz>
2016-09-25 16:25:20 +0100

    added logit

--- alter_data.py
+++ alter_data.py
@@ -1,29 +1,57 @@
 import pandas as pd
+from tqdm import tqdm
 
 df_min = pd.read_csv('minutes.csv')
 df_res = pd.read_csv('results.csv')
 
 
-d = {'Home_goals':None, 'Away_goals':None, 'Times':None, 'Winners':None}
+d = {'home_score':None, 'away_score':None, 'time':None, 'winners':None}
 df_final = pd.DataFrame(data=d, index=range(0))
-for row in df_res.iterrows():
+
+i = 0
+j = 0
+for row in tqdm(df_res.iterrows()):
         
         row = row[1]       
- 
+
+	if row.home_goals == 0 and row.away_goals == 0:
+		continue
+	
         home_team = row.home_team    
         away_team = row.away_team
-        date      = row.date
+	date      = row.date
 
-        date_mask  = df_mins['date'] == date
-        team_mask1 = ((df_mins['scoring_team'] == home_team) & 
-                              (df_mins['conceding_team'] == away_team)) 
-        team_mask2 = ((df_mins['scoring_team'] == away_team) & 
-                              (df_mins['conceding_team'] == home_team))                  
-        mask = team_mask1 | team_mask2     
+	if row.home_goals > row.away_goals:
+
+		winners = 1
+	elif row.away_goals > row.home_goals:
+
+		winners = -1
+	else:
+
+		winners = 0
+
+
+	date_mask = df_min['date'] == date
+        team_maskh = ((df_min['scoring_team'] == home_team) & 
+                              (df_min['conceding_team'] == away_team)) 
+        team_maska = ((df_min['scoring_team'] == away_team) & 
+                              (df_min['conceding_team'] == home_team))         
+
+	team_mask = team_maska | team_maskh     
 
         full_mask = date_mask & team_mask
+	sub_frame = df_min[full_mask]
+		
+	home_score, away_score = 0, 0
+	for sub_row in sub_frame.iterrows():
+		sub_row = sub_row[1]
 
-        df_match = df_mins[full_mask]
+		if sub_row.scoring_team == home_team:
+			home_score += 1
+		if sub_row.scoring_team == away_team:
+			away_score += 1
 
-        
+		df_final.loc[i] = [home_score, away_score, sub_row.minute, winners]
 
+		i += 1
--- win_reg.py
+++ win_reg.py
@@ -1,1 +1,38 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_score
+import patsy as pt
+import numpy as np
 
+def win_logit(home_score, away_score, time, team='home'):
+
+	df = pd.read_csv('logit_data.csv')
+
+	if team == 'away':
+		
+		df.winners = -1 * df.winners
+	
+	df.winners = (df.winners + abs(df.winners)) / 2	
+
+	y, X = pt.dmatrices('winners ~ home_score + away_score + time',
+                  df, return_type="dataframe")
+
+
+	y = np.ravel(y)
+
+	model = LogisticRegression()
+	model = model.fit(X, y)
+
+	scores = cross_val_score(LogisticRegression(), X, y, scoring='accuracy', cv=10)
+	print "The cross-validated model score is: %.2f%%" %(100*scores.mean())
+
+
+	results =  model.predict_proba(np.array([1, home_score, away_score, time]))
+	print("Given a score of %d - %d at %d minutes, the probability of the %s team winning is %.2f%%"
+		%(home_score, away_score, time, team, 100*results[0][1]))	
+
+
+
+if __name__ == '__main__':
+
+	win_logit(1, 3, 30)
